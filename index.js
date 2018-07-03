f481e290c7af58e51703daa1535bce2207a194eb
Eugene Lerman <eugenel@wix.com>
2018-07-03 19:54:52 +0300

    formatting

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-'use strict';
-const alfy = require('alfy');
+'use strict'
+const alfy = require('alfy')
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const BASE_URL = 'https://www.wix.com/code/reference'
+const API_REFERENCE_URL = `${BASE_URL}/scripts/search.data.json`
+
+const formatTitle = ({ title, subtitle }) => `${subtitle}.${title}`
+const formatArg = ({ url }) => `${BASE_URL}/${url}`
+
+const formatResult = result => ({
+	uid: formatTitle(result),
+	title: formatTitle(result),
+	subtitle: result.content,
+	arg: formatArg(result),
+	quicklookurl: formatArg(result),
+	match: formatTitle(result),
+})
+
+alfy.fetch(API_REFERENCE_URL).then(results => {
+	alfy.output(results.map(formatResult))
+})
